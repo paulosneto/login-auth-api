2db3a361e69fb8a12c2b88ecc46a9f503582949d
paulo_sneto <paulo_sneto@yahoo.com>
2024-04-25 16:58:14 -0300

    feat: create LoginAuthApiApplicationTests.java

---
+++ login-auth-api/src/test/java/combr/loginauthapi/LoginAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package combr.loginauthapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
