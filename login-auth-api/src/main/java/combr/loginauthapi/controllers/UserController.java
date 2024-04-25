edcfa05dc4dd01b532ea34931910b4b6fc2244fd
paulo_sneto <paulo_sneto@yahoo.com>
2024-04-25 16:55:25 -0300

    feat: create TokenService.java

---
+++ login-auth-api/src/main/java/combr/loginauthapi/controllers/UserController.java
@@ -0,0 +1,20 @@
+package combr.loginauthapi.controllers;
+
+import jakarta.persistence.GeneratedValue;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping
+    public ResponseEntity<String> getUsers(){
+        return ResponseEntity.ok("sucesso!");
+
+    }
+
+}
